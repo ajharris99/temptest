218375e4d225a279cfe96d10ddaf244a0d7f5e0d
Andrew Harris <ajharris99@ntlworld.com>
2018-08-01 19:58:58 +0100

    This is the initial shop without the special offers.
    Call the program with a list of words "Apple" or "Orange" (case insensitive, leading substrings are sufficient)
    E.g. Ap orang APPLE could count as 2 apples and one orange

--- src/applesOranges/ApplesOranges.java
+++ src/applesOranges/ApplesOranges.java
@@ -10,6 +10,7 @@ package applesOranges;
  * @author Andrew
  */
 public class ApplesOranges {
+   static int applePrice = 60, orangePrice = 25;
 
     /**
      * @param args the command line arguments.  A list of leading substrings
@@ -17,7 +18,6 @@ public class ApplesOranges {
      */
     public static void main(String[] args) {
         int nApples = 0, nOranges = 0;
-        int applePrice = 60, orangePrice = 25;
 
         for (String item : args) {
             if (!item.isEmpty()) {
@@ -33,11 +33,15 @@ public class ApplesOranges {
             }
         }
         
-        // Special offer modification
-
+        checkout(nApples, nOranges);
+    }
+    
+    private static void checkout(int nApples, int nOranges) {
         System.out.format("Total cost of %d apple%s and %d orange%s is £%.2f\n",
                 nApples, (nApples == 1 ? "" : "s"),
                 nOranges, (nOranges == 1 ? "" : "s"),
-                ((nApples - nApples / 2) * applePrice + (nOranges - nOranges / 3) * orangePrice)/100.0);
+                (nApples  * applePrice + nOranges * orangePrice)/100.0);
+        // Special offer modification
+        //        ((nApples - nApples / 2) * applePrice + (nOranges - nOranges / 3) * orangePrice)/100.0);
     }
 }
